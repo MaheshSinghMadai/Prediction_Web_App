7c01a4e83988e9de1aee24ad4c6c1dfdb54ce1ee
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-06-06 14:05:55 +0545

    layers setup

---
+++ Prediction_Web_App.Core/Entities/Player_Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prediction_Web_App.Core.Entities
+{
+    public  class Player_Info
+    {
+        [Key]
+        public int Player_ID { get; set; }
+        public int Player_Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Prediction_Web_App.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prediction_Web_App.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+}
