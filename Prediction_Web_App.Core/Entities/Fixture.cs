22c6bd54954932f1b5fc2dabb8da2d2b8a511e0c
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-06-11 17:28:27 +0545

    country component initial

--- Prediction_Web_App.Core/Entities/Fixture.cs
+++ Prediction_Web_App.Core/Entities/Fixture.cs
@@ -7,8 +7,10 @@ namespace Prediction_Web_App.Core.Entities
         [Key]
         public int Fixture_ID { get; set; }
         public string Country1 { get; set; }
+        public string Country1_Flag { get; set; }
         public int Country1_Score { get; set; }
         public string Country2 { get; set; }
+        public string Country2_Flag { get; set; }
         public int Country2_Score { get; set; }
         public string Result { get; set; }
         public ICollection<Goal_Scorer> Goal_Scorers { get; set; }
---
+++ Prediction_Web_App.Infrastructure/Migrations/20240610120515_Flag url added.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prediction_Web_App.Infrastructure.Migrations
+{
+    public partial class Flagurladded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country1_Flag",
+                table: "Fixtures",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country2_Flag",
+                table: "Fixtures",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country1_Flag",
+                table: "Fixtures");
+
+            migrationBuilder.DropColumn(
+                name: "Country2_Flag",
+                table: "Fixtures");
+        }
+    }
+}
